3a47eeba1012cf96dcc254bd157c6e8179213d00
lipa <tszkaradek@student.agh.edu.pl>
2023-06-07 21:29:42 +0200

    'test'

--- config/jenkins.Dockerfile
+++ config/jenkins.Dockerfile
@@ -1,5 +1,19 @@
 FROM jenkins/jenkins:lts
 
+COPY vnc_config.sh /var/jenkins_home/vnc_config.sh
+COPY requirements.txt /var/jenkins_home/requirements.txt
+COPY chromedriver.exe /var/jenkins_home/chromedriver.exe
+
+USER root
+
+RUN chmod +x /var/jenkins_home/vnc_config.sh && /var/jenkins_home/vnc_config.sh
+
+USER jenkins
+
+# USER root
+# RUN chmod +x /var/jenkins_home/vnc_config.sh
+# USER jenkins
+
 # FROM jenkinsci/blueocean
 # COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 # COPY script.groovy /usr/share/jenkins/ref/init.groovy.d/setup.groovy
---
+++ config/vnc_config.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+apt-get update -y
+apt-get install nano -y
+apt-get install x11-apps -y
+apt-get install tightvncserver -y
+apt-get install wget -y
+
+
+# Setup VNC server
+mkdir -p ~/.vnc && echo -n "adminadmin" | vncpasswd -f > ~/.vnc/passwd
+chmod 600 ~/.vnc/passwd
+USER=admin vncserver :1 -auth /root/.Xauthority -geometry 1024x768 -depth 24 -rfbwait 120000 -rfbauth /root/.vnc/passwd -fp /usr/share/fonts/X11/misc/,/usr/share/fonts/X11/Type1/,/usr/share/fonts/X11/75dpi/,/usr/share/fonts/X11/100dpi/ -co /etc/X11/rgb
+export DISPLAY=:1
+xhost +
+# ps aux | grep Xtightvnc
+
+
+# Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+apt-get install -f -y
+dpkg -i google-chrome-stable_current_amd64.deb
+# google-chrome --version
+
+
+# Install ChromeDriver
+CHROME_DRIVER_VERSION=`curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE`
+wget https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+rm chromedriver_linux64.zip
+mv chromedriver /usr/bin/chromedriver
+chown root:root /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
+apt-get install -y libglib2.0-0
+apt-get install -y libnss3
+# chromedriver --version
+
+
+# Install Python
+apt-get update && apt-get install -y python3 python3-pip
+if [ -f /var/jenkins_home/requirements.txt ]; then
+    pip install -r /var/jenkins_home/requirements.txt
+else
+    echo "Nie znaleziono pliku /var/jenkins_home/requirements.txt"
+fi
