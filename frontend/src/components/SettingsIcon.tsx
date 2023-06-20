f4c089b82fdc2154aeec36c2f84c439914e99a5a
lipa <tszkaradek@student.agh.edu.pl>
2023-06-20 19:08:33 +0200

    'commit'

--- frontend/src/components/SettingsIcon.tsx
+++ frontend/src/components/SettingsIcon.tsx
@@ -3,11 +3,23 @@ import { OverlayPanel } from "primereact/overlaypanel";
 import { Menu } from "primereact/menu";
 import { ThemeContext } from "../context/ThemeContext";
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from "react-router-dom";
+import useLogout from "../hooks/useLogout";
+import { useToast } from "../context/ToastProvider";
 
 const SettingsIcon = () => {
   const op = useRef<OverlayPanel | null>(null);
   const { theme, currentTheme, toggleTheme } = useContext(ThemeContext);
   const { t } = useTranslation('translations');
+  const { showToast } = useToast();
+  const navigate = useNavigate();
+  const logout = useLogout();
+
+  const signOut = async () => {
+      await logout();
+      navigate('/');
+      showToast("success", "Success", "Logout successfully");
+  }
 
   const settings = [
     {
@@ -21,7 +33,7 @@ const SettingsIcon = () => {
       label: t("Logout"), 
       icon: "pi pi-fw pi-sign-out",
       command: () => {
-        toggleTheme(theme === 0 ? 1 : 0);
+        signOut()
       },
     },
   ];
--- tests/test.py
+++ tests/test.py
@@ -21,13 +21,13 @@ class TestMyWebsite:
     def test_check_help_message(self, driver):
         driver.get("http://frontend:5173")
         time.sleep(2)
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="navbar-nav"]/div[1]/a[2]')))
-        button = driver.find_element(By.XPATH,'//*[@id="navbar-nav"]/div[1]/a[2]')
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div[1]/nav/ul/li[2]/a')))
+        button = driver.find_element(By.XPATH,'//*[@id="root"]/div[1]/nav/ul/li[2]/a')
         button.click()
         time.sleep(2)
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div')))
-        message = driver.find_element(By.XPATH,'//*[@id="root"]/div/div').text
-        assert message == "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div[1]/div')))
+        message = driver.find_element(By.XPATH,'//*[@id="root"]/div[1]/div').text
+        assert message == "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut elit a lectus euismod pretium ut in ipsum. Aliquam velit nisi, lobortis pellentesque sodales et, ornare eget augue. Nulla facilisi. Sed eleifend, neque sit amet posuere dictum, dolor eros luctus magna, vel maximus neque ipsum et diam. Quisque vulputate dapibus tortor quis fermentum. Nullam accumsan non nibh in placerat. Suspendisse et dolor congue dolor congue tempus eget in enim."
 
     def test_1(self, driver):
         assert True
